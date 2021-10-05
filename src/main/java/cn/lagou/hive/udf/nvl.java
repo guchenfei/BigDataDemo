74483c13c305acc24ce224d2c057bd0f0cf9acf4
guchenfei <389443466@qq.com>
2021-10-05 19:54:20 +0800

    add update nvl function

---
+++ src/main/java/cn/lagou/hive/udf/nvl.java
@@ -0,0 +1,13 @@
+package cn.lagou.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public class nvl extends UDF {
+    public Text evaluate(final Text x, final Text y) {
+        if (x == null || x.toString().trim().length() == 0) {
+            return y;
+        }
+        return x;
+    }
+}
