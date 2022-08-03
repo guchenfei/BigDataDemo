1b969e3f6fac398048e644fec5eac7bf8621e516
Chenfei Gu <guchenfei@papayamobile.cn>
2022-08-03 15:30:10 +0800

    update demo

--- src/main/java/flink/join/Demo.java
+++ src/main/java/flink/join/Demo.java
@@ -4,16 +4,16 @@ import com.alibaba.fastjson.JSON;
 
 public class Demo {
     public static void main(String[] args) {
-//        UserInfo userInfo = new UserInfo();
-//        userInfo.setUserName("google");
-//        userInfo.setCityId(1000);
-//        userInfo.setTs(1L);
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserName("google");
+        userInfo.setCityId(1000);
+        userInfo.setTs(1L);
 
-        CityInfo cityInfo = new CityInfo();
-        cityInfo.setCityId(1000);
-        cityInfo.setCityName("北京");
-        cityInfo.setTs(1L);
+//        CityInfo cityInfo = new CityInfo();
+//        cityInfo.setCityId(1000);
+//        cityInfo.setCityName("北京");
+//        cityInfo.setTs(1L);
 
-        System.out.println(JSON.toJSONString(cityInfo));
+        System.out.println(JSON.toJSONString(userInfo));
     }
 }
